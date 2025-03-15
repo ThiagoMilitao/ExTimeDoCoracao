f315e8b1ad9d6ceaf651a6e3e04ef6f8454491a4
Thiago Militão <thiago.militao2007@gmail.com>
2025-03-14 23:19:09 -0300

    Maior time do Brasil

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string time = "Palmeiras";
+        ConsoleColor[] cores = { ConsoleColor.Green, ConsoleColor.White }; // Alternando entre verde e branco
+
+        for (int i = 0; i < time.Length; i++)
+        {
+            Console.BackgroundColor = cores[i % cores.Length]; // Define a cor de fundo
+            Console.ForegroundColor = cores[(i + 1) % cores.Length]; // Usa a outra cor para o texto
+            Console.Write(time[i]); // Exibe a letra
+        }
+        Console.WriteLine("");
+        Console.ResetColor();
+        Console.WriteLine("Maior time do Brasil");
+
+
+    
